698942eb10da73087cd7b38006779581fc145851
Med. Amine Aljane <73314263+MedAljane@users.noreply.github.com>
2021-08-26 00:04:47 +0100

    Update transPhrasing.py
    - Added simple testing function for invalid generated sentences (a bug that repeats some words a couple of times)

--- transPhrasing.py
+++ transPhrasing.py
@@ -4,16 +4,33 @@ from textblob import TextBlob
 from deep_translator import GoogleTranslator
 from sentence_splitter import SentenceSplitter, split_text_into_sentences
 
+def sentenceIsValid(sentence):
+    words = sentence.split(' ')
+    for element in words:
+        if len(element) > 3 and words.count(element) > 7:
+            return False
+        else:
+            return True
+
 def transPhrasing(text):
     splitter = SentenceSplitter(language=TextBlob(text).detect_language())
     sentence_list = splitter.split(text)
     translated_sentences = [GoogleTranslator(target='en').translate(element) for element in sentence_list]
-    paraphrased_sentences = [get_response(e, 1, 1) for e in translated_sentences]
-    cleaned_paraphrased_sentences = [e[0] for e in paraphrased_sentences]
-    paraphrased_result_En = [' '.join(x for x in cleaned_paraphrased_sentences)]
-    paraphrased_result_cleaned_En = str(paraphrased_result_En).strip('[]')
+
+
+    paraphrased_sentences = []
+
+    for e in translated_sentences:
+        result = get_response(e, 1, 1)
+        if sentenceIsValid(result[0]):
+            paraphrased_sentences.append(result[0])
+        else:
+            paraphrased_sentences.append(e)
+
+    paraphrased_result_En = [' '.join(x for x in paraphrased_sentences)]
+    paraphrased_result_cleaned_En = str(paraphrased_result_En).strip('[]').strip("'").strip('"')
     
-    sentences_to_french = [GoogleTranslator(target='fr').translate(e) for e in cleaned_paraphrased_sentences]
+    sentences_to_french = [GoogleTranslator(target='fr').translate(e) for e in paraphrased_sentences]
     paraphrased_result_fr = [' '.join(x for x in sentences_to_french)]
     paraphrased_result_cleaned_fr = str(paraphrased_result_fr).strip('[]').strip("'").strip('"')
 

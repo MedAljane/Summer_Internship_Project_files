89a94ce1c50cf35660889efd8cdcc9a0eb32e47e
Med. Amine Aljane <73314263+MedAljane@users.noreply.github.com>
2021-08-28 09:31:46 +0100

    Update pegasus_paraphrase.py
    - Added documentation to functions
    -  Changed some variable names

--- pegasus_paraphrase.py
+++ pegasus_paraphrase.py
@@ -1,22 +1,50 @@
-import torch
-import os
-# Insert the directory for the CUDA binary files if you got a warning implying that TesorFlow can't use CUDA
-# because of missing library Files
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.4/bin")
+import pegasus_paraphrase
+from pegasus_paraphrase import get_response
+from textblob import TextBlob
+from deep_translator import GoogleTranslator
+from sentence_splitter import SentenceSplitter, split_text_into_sentences
 
-from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
-# Set the 'model_name' variable's velue to the path of the folder containing the files of the model
-# or set it to 'tuner007/pegasus_paraphrase' if you wish to download the files each time the script is run
-model_name = 'C:\\Users\\mhmda\\Desktop\\Project files\\Paraphraser files\\'
+def sentence_is_valid(sentence) -> bool:
+    """
+    Determine if a generated sentence is valid or not
+    :param sentence: String
+    :return: Bool
+    """
+    words = sentence.split(' ')
+    for element in words:
+        if len(element) > 3 and words.count(element) > 7:
+            return False
+        else:
+            continue
+    return True
 
-# check if CUDA is available and if the available VRAM is equal to or higher than 6Gb (8Gb is recommended)
-torch_device = 'cuda' if torch.cuda.is_available() and torch.cuda.get_device_properties(0).total_memory >= 6442450944 else 'cpu'
-tokenizer = PegasusTokenizer.from_pretrained(model_name)
-model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
 
-def get_response(input_text,num_return_sequences,num_beams):
-  batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
-  translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
-  tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-  return tgt_text
+def trans_phrasing(text) -> list:
+    """
+    Translate a text, split it to sentences and generates paraphrased sentences using the generating function,
+    returns a list containing the paraphrased text in both English and French languages
+    :param text: String
+    :return: List
+    """
+    splitter = SentenceSplitter(language=TextBlob(text).detect_language())
+    sentence_list = splitter.split(text)
+    translated_sentences = [GoogleTranslator(target='en').translate(element) for element in sentence_list]
+
+    paraphrased_sentences = []
+
+    for e in translated_sentences:
+        result = get_response(e, 1, 1)
+        if sentence_is_valid(result[0]):
+            paraphrased_sentences.append(result[0])
+        else:
+            paraphrased_sentences.append(e)
+
+    paraphrased_result_en = [' '.join(x for x in paraphrased_sentences)]
+    paraphrased_result_cleaned_en = str(paraphrased_result_en).strip('[]').strip("'").strip('"')
+    
+    sentences_to_french = [GoogleTranslator(target='fr').translate(e) for e in paraphrased_sentences]
+    paraphrased_result_fr = [' '.join(x for x in sentences_to_french)]
+    paraphrased_result_cleaned_fr = str(paraphrased_result_fr).strip('[]').strip("'").strip('"')
+
+    return [paraphrased_result_cleaned_en, paraphrased_result_cleaned_fr]
